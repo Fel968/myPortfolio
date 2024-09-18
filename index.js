1288103410cc1a2a9f583bb72524f223b24c078e
Felicity Semetey <felicitysemetey628@gmail.com>
2024-09-18 01:03:24 -0500

    hero progress

---
+++ index.js
@@ -0,0 +1,14 @@
+// const navbar = document.querySelector('nav');
+// const stickyOffset = navbar.offsetTop;
+// function toggleSticky() {
+//     if (window.pageYOffset >= 100) {
+//       navbar.classList.add("sticky");
+//     } else {
+//       navbar.classList.remove("sticky");
+//     }
+//   }
+  
+//   // Add scroll event listener
+//   window.onscroll = function() {
+//     toggleSticky();
+//   };
